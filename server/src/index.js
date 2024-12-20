46b3faf8e3b8c6a16f95f8a9eb4cf41aaa7f14b9
diptu <diptunazmulalam@gmail.com>
2024-12-20 22:01:00 +0600

    mongoDb connection created

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,10 @@
 const app = require('./app');
 const { SERVER_PORT } = require('./secret');
+const connectionDB = require('./config/db');
 
-app.listen(SERVER_PORT, ()=>{
+
+app.listen(SERVER_PORT, async ()=>{
     console.log(`server running on http://localhost:${SERVER_PORT}`);
-});
+     await connectionDB()
+})
 
--- server/src/secret.js
+++ server/src/secret.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 const SERVER_PORT = process.env.SERVER_PORT || 3001;
+const DB_NAME = process.env.DB_NAME || 'gadgetsHaven';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || `mongodb://localhost:27017/${DB_NAME}`;
+
 
 module.exports = {
     SERVER_PORT,
+    MONGO_DB_URL,
 }
