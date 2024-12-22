682aeb19158632193dd3f85b579490d61cd14c04
diptu <diptunazmulalam@gmail.com>
2024-12-22 21:05:36 +0600

    xss middleware added

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan')
 const createError = require('http-errors')
 const rateLimit = require('express-rate-limit')
 const app = express();
-// const xss = require("xss"); 
+const { xss } = require('express-xss-sanitizer');
 
 const limiter = rateLimit({
 	windowMs: 5 * 60 * 1000, // 5 minutes
@@ -16,7 +16,7 @@ const limiter = rateLimit({
 
 // Apply the rate limiting middleware to all requests.
 app.use(limiter)
-// app.use(xss());
+app.use(xss());
 app.use(morgan("dev"));
 
 app.use(express.json());
@@ -25,25 +25,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed',seedRouter);
 app.use('/api/products',productRoute);
 
+app.get('/api/user', (req, res, next)=>{
+  res.status(200).send({message: 'User Profile!'});
+});
 app.get('/', (req, res)=>{
-
-    res.statusCode = 200;
-    res.send('Welcome to home');
+    res.status(200).send({message: 'welcome Home!'});
+ 
 });
 
-
-
 // client error handling
-app.use(( req, res, next) => {    
+app.use(( req, res, next) => { 
+    // http-errors   
     next(createError(404, 'Route Not Found')); // check error
   })
 
-
-
-
-  // server error handling
+  // server error handling => all errors will be logged here
   app.use((err, req, res, next) => {
-    // console.error(err.stack)
      return res.status(err.status || 500).json({
         success : false,
         message : err.message
